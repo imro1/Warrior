b967e172192f1f03850a268e168cb459a48013e1
Mubeen Khan <mubkhan01@gmail.com>
2021-11-29 14:44:44 -0500

    trying to fix the merge

--- Warrior_Game/Level1.java
+++ Warrior_Game/Level1.java
@@ -28,45 +28,37 @@ public class Level1 extends World
            addObject( new enemySoldier(),950,540);
            waiting = true;
         } 
+        
     }
     public void act()
     {
-       if (waiting) {
-       delay++;
-       if (delay >= 200) { // you'll have to pick a number for limit
+        if (waiting) {
+            delay++;
+            if (delay >= 200) { // you'll have to pick a number for limit
            
-        int x = 950; 
-        int y = 540;
-        //int y = 530;
-        addObject( new enemySoldier(), x, y );
-        addObject( new enemySoldier(),0,540);
-        waiting = true;
-        delay = 0;
-        // you may set waiting to true or false depending on if you want to add more objects or just one
-       }
-       } 
-        
-        //Enemy Soldiers
-      /*  if (Greenfoot.getRandomNumber(100) < 5) {
-             addObject( new  enemySoldier(), 50, 530);
+                int x = 950; 
+                int y = 540;
+                //int y = 530;
+                addObject( new enemySoldier(), x, y );
+                addObject( new enemySoldier(),0,540);
+                waiting = true;
+                delay = 0;
+                // you may set waiting to true or false depending on if you want to add more objects or just one
             }
-        if (Greenfoot.getRandomNumber(1000) < 5) {
-            addObject( new  enemySoldier(), 950,530);
-        }
-        */
-     
+        } 
+
         //Enemy Planes
         if (Greenfoot.getRandomNumber(1000) <= 3) {
              addObject( new  Plane(), 1, Greenfoot.getRandomNumber(40)+40);
              
-            }
+        }
         /*if (Greenfoot.getRandomNumber(1000) <= 5) {
             addObject( new  Plane(), 1000,80);
         }   */         
     }
 
     public void prepare() {
-        
+
         Soldier soldier = new Soldier();
         addObject(soldier,149,515);
         ///////////////////////////////////////////////////////////
@@ -79,5 +71,13 @@ public class Level1 extends World
         addObject(brick2,450,420);
         brick2.setLocation(450,420);
 
+        soldier.setLocation(436,515);
+        soldier.setLocation(430,14);
+
+        Border border = new Border();
+        addObject(border,30,525);
+        border.setLocation(30, 525);
+
+        
     }
 }
