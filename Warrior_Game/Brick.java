db66782619d6d63abb1a4966be916ac3ea040d04
Mubeen Khan <mubkhan01@gmail.com>
2021-12-06 14:54:57 -0500

    trying to upload the previous changes again

--- Warrior_Game/Brick.java
+++ Warrior_Game/Brick.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Brick extends ExtraStuff
+public class Brick extends Obstacle
 {
     public Brick(){
         this(100,25);
