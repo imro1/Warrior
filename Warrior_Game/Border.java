e029d88ea4a3e512ec962d2e7678c0e163be9d93
Mubeen Khan <mubkhan01@gmail.com>
2021-12-06 15:01:32 -0500

    adding border and obstable file

---
+++ Warrior_Game/Border.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Border here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Border extends Obstacle
+{
+    /**
+     * Act - do whatever the Border wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
